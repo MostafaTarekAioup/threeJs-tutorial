7f969e1b492c6824646b87d53252f5e89fb049f9
mmostafa.c <mostafatarekaioup@gmail.com>
2025-04-23 11:44:10 +0200

    play with some animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 import "./App.css"
 
 function App() {
+  const BoxAnimation = () => {
+    const boxRef = useRef(null)
+    useFrame(() => {
+      // @ts-expect-error - current.rotation is not recognized on MeshRef type
+      boxRef.current.rotation.x += 0.005
+      // @ts-expect-error - current.rotation is not recognized on MeshRef type
+      boxRef.current.rotation.y += 0.005
+      // @ts-expect-error - current.rotation is not recognized on MeshRef type
+      boxRef.current.rotation.z += 0.005
+    })
+    return (
+      <mesh
+        // position={[-2, 2, 1]}
+        // rotation={[0, 0, 5]}
+        // scale={[2, 2, 2]}
+        ref={boxRef}
+      >
+        {/* <sphereGeometry args={[2, 80, 80]} /> */}
+        {/* <torusKnotGeometry args={[1.3, 0.3, 256, 256]} /> */}
+        <boxGeometry args={[2, 3, 2]} />
+        <meshToonMaterial color='red' />
+      </mesh>
+    )
+  }
   return (
     <div id='canvas-container' style={{ width: "100vw", height: "100vh" }}>
       <Canvas
         style={{ backgroundColor: "lightBlue" }}
         camera={{ position: [2, 2, 2] }}
       >
-        <mesh
-        // position={[-2, 2, 1]}
-        // rotation={[0, 0, 5]}
-        // scale={[2, 2, 2]}
-        >
-          {/* <sphereGeometry args={[2, 80, 80]} /> */}
-          <boxGeometry args={[2, 3, 2]} />
-          {/* <torusKnotGeometry args={[1.3, 0.3, 256, 256]} /> */}
-          <meshToonMaterial color='red' />
-          <directionalLight position={[2, 5, 1]} />
-        </mesh>
+        <BoxAnimation />
+        <directionalLight position={[2, 5, 1]} />
       </Canvas>
     </div>
   )
